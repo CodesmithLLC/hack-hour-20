7b1be2a9298d4e6b68f7e6e517662e0e7331a000
Riyad Shauk <riyad.shauk@gmail.com>
2018-03-03 15:06:25 -0800

    submission and added passing test cases

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -10,8 +10,17 @@
  */
 
 
-function modemean(array) {
-
-}
+const modemean = (a, freqs = {}) => {
+  if (!a.length) return true; // vacuously
+  for (let i = 0; i < a.length; i++) {
+    if (freqs[a[i]]) freqs[a[i]]++;
+    else freqs[a[i]] = 1;
+  }
+  let mode = a[0];
+  for (let i = 1; i < a.length; i++) if (freqs[a[i]] > freqs[mode]) mode = a[i];
+  const acc = a.reduce((acc, e) => acc + e, 0);
+  mode = freqs[Math.floor(acc / a.length)] > freqs[mode] ? Math.floor(acc / a.length) : mode;
+  return Math.floor(acc / a.length) === mode;
+};
 
 module.exports = modemean;
--- test/index.js
+++ test/index.js
@@ -12,3 +12,11 @@ describe('draw-stairs', () => {
     expect(false).to.eql(true);
   });
 });
+
+describe('mode-mean', () => {
+  it('should return true vacuously', () => expect(modemean([])).to.eql(true));
+  it('should return true with one element', () => expect(modemean([0])).to.eql(true));
+  it('should return true with exactly two elements s.t. they are distinct', () => expect(modemean([1, 2])).to.eql(true));
+  it('should return true with exactly two elements s.t. they are NOT distinct', () => expect(modemean([1, 2])).to.eql(true));
+  it('should return false with exactly three elements s.t. they are distinct', () => expect(modemean([1, 2])).to.eql(true));
+});
