c2a82e61601c793127961f840ecaa2705287cb06
Luis <33737863+langelramirez@users.noreply.github.com>
2018-03-03 19:29:44 -0800

    update challenge 3

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,24 +11,25 @@
 
 
 function modemean(array) {
-    let set = new Set (array);
-    set
-    let obj = {};
-    let theModeKey;
-    [...set].forEach(num => {obj[num] = undefined})
-     set.forEach(key =>{
-        if (obj[String(key)] === undefined) {
-          obj[String(key)] = 1;
-          console.log(obj[String(key)]);
-        } else if (obj[String(key)]) {
-          console.log('hello')
-            obj[String(key)] = +1
-            if (obj[String(key)] > obj[theModeKey]) {
-               theModeKey = String(key);
-            }
-        }
-    });
-}
+  let set = new Set (array);
+  let obj = {};
+  let theModeKey = 0;
+  [...set].forEach(num => {obj[num] = undefined});
 
+   array.forEach(key =>{
+      if (obj[key] === undefined) obj[String(key)] = 1;
+      else if (obj[key]) {
+          obj[key]++;
+          if(!obj[theModeKey]) theModeKey = key;
+          if (obj[key] > obj[theModeKey] ) theModeKey = key;
+          if(obj[key] === obj[theModeKey] && key > theModeKey) theModeKey = key;
+      }
+  });
+  
+  return Math.floor(array.reduce((acc, cur) => {
+    return acc + cur;
+  })/2) === theModeKey;
+}
+ 
 
 module.exports = modemean;
