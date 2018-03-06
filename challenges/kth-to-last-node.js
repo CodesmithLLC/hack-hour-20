3da7f2bdd32f47faba6e75a2e224deaa10d61981
josephwu1994 <josephwu1994@gmail.com>
2018-03-06 09:42:43 -0800

    kth to last node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -1,20 +1,8 @@
 /**
  * Write a function that takes two parameters, an integer and the head of a
  * singly linked list, and returns the VALUE on the kth to last node in the list.
- *
- * const a = new Node('A');
- * const b = new Node('B');
- * const c = new Node('C');
- * const d = new Node('D');
- * const e = new Node('E');
- *
- * a.next = b;
- * b.next = c;
- * c.next = d;
- * d.next = e;
- *
- * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
+ 
 
 function Node(val) {
   this.value = val;
@@ -22,7 +10,27 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  let count = 0;
+  let copy = head;
+  while(copy !== null){
+    copy = copy.next;
+    count++;
+  }
+  for(let i = 0 ; i < count-k;i++){
+    head = head.next;
+  }
+  return head;
 }
+
+//  const a = new Node('A');
+//  const b = new Node('B');
+//  const c = new Node('C');
+//  const d = new Node('D');
+//  const e = new Node('E');
+//  a.next = b;
+//  b.next = c;
+//  c.next = d;
+//  d.next = e;
+//  console.log(kthToLastNode(2, a));
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
