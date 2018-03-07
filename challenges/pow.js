e2d218e6feb308c4567ff02d7013c96dc8f69faf
josephwu1994 <josephwu1994@gmail.com>
2018-03-07 09:49:10 -0800

    Alternate Solution

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,13 +2,18 @@
  * Use recursion!
  */
 
-function pow(base, power, total = 1) {
-    if(power === 1){
-        return base;
-    }else{ 
-        total = pow(base,power-1,total);
-        return total*base;
+function pow(base, power) {
+    // if(power === 1){
+    //     return base;
+    // }else{ 
+    //     total = pow(base,power-1,total);
+    //     return total*base;
+    // }
+    let total = 1;
+    for(let i =0 ;i<power;i++){
+        total*=base;
     } 
+    return total;
 }
-console.log(pow(5,10));
+console.log(pow(2,10));
 module.exports = pow;
