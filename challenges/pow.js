0b0ab9e526ab2551f5b1b2511674a2bf7f0e5ddd
Ben Gummelt <camaromelt@gmail.com>
2018-03-09 09:31:32 -0800

    refactored code

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,15 +2,10 @@
  * Use recursion!
  */
 
-function pow(base, power, result= base) {
-if(power === 1){
-    return result;
-}
-if(power === 0){
-    return 1;
-}
+function pow(base, power, result = base) {
+if(power === 1) return result;
+if(power === 0) return 1;
 return pow(base, --power, base * result)
 }
 
 module.exports = pow;
-console.log(pow(3,0))
