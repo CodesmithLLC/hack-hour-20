d0e5b57cd26de8d9639b37ceed77bc93f64c71d7
Esther Kim <estherkim@MacBook-Air-2075.local>
2018-03-13 10:03:13 -0700

    T^T

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,23 @@
  */
 
 function balancedParens(input){
-
+  let count = 0;
+//if string is has parentheses or brackets one most have another
+//if string includes ( then it must also have )
+//^ same with brackets
+//make a counter to count the () and the [] and the {}
+if(input.includes('(' && ')')) {
+  return true;
+} else if(input.includes('[' && ']')) {
+  return true;
+} else if(input.includes('{' && '}')) {
+  return true;
+} //else if((paren + square + curly) % 2 === 0) {
+  //return true;
+return false;
 }
+
+
+console.log( balancedParens('())'));
 
 module.exports = balancedParens;
