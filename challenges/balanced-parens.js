3f123d6cca4d55c5cbec89dd8be09f9399fb5a20
Esther Kim <estherkim@MacBook-Air-2075.local>
2018-03-13 10:07:08 -0700

    first case done

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,16 +25,15 @@
  */
 
 function balancedParens(input){
-  let count = 0;
 //if string is has parentheses or brackets one most have another
 //if string includes ( then it must also have )
 //^ same with brackets
 //make a counter to count the () and the [] and the {}
-if(input.includes('(' && ')')) {
+if(input.includes('(') && input.includes(')')) {
   return true;
-} else if(input.includes('[' && ']')) {
+} else if(input.includes('[') && input.includes(']')) {
   return true;
-} else if(input.includes('{' && '}')) {
+} else if(input.includes('{') && input.includes('}')) {
   return true;
 } //else if((paren + square + curly) % 2 === 0) {
   //return true;
@@ -42,6 +41,6 @@ return false;
 }
 
 
-console.log( balancedParens('())'));
+console.log( balancedParens('{}'));
 
 module.exports = balancedParens;
