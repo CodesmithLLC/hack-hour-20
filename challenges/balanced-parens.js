6f208838314b09f164a369686f70007951739361
Phillip Troutman <philliptroutman21@gmail.com>
2018-03-15 08:51:13 -0700

    catching up on hackhour

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,32 @@
  *
  */
 
-function balancedParens(input){
-
+function balancedParens(input) {
+    let openings = ["(", "[", "{"]; 
+    let closings = [")", "]", "}"]; 
+    let result = [];
+    let foo = input.split('');
+    for(let i = 0; i < foo.length; i++){
+        if(openings.includes(foo[i])){
+            result.push(foo[i]);
+        }else if(closings.includes(foo[i]) && result[result.length-1] === openings[closings.indexOf(foo[i])]){
+            result.pop(); 
+        }
+    }
+    return result.length === 0; 
 }
+console.log("These should be true: ");
+console.log(balancedParens('()')); // true
+console.log(balancedParens('(())'));  // true
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]')); // true
+console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
+
+console.log("These should be false: ");
+console.log(balancedParens('('));  // false
+console.log(balancedParens(')('));  // false
+console.log(balancedParens('[(]{)}')); // false
+console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
+
 
 module.exports = balancedParens;
