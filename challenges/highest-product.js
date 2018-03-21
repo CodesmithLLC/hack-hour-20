9a7bbb631571a63bfafd7ed7aa14e4ea0606a3db
Luis <33737863+langelramirez@users.noreply.github.com>
2018-03-21 10:05:29 -0700

    update product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -9,10 +9,14 @@ function highestProduct(array) {
     if (array.length<3) return 0;
     if (!Array.isArray(array)) return 0;
 
-    let productOfTwo = 0;
-    let productOfThree = 1;
+    let productOfTwo;
+    let productOfThree;
      for (let i = 0; i < array.length; i++){
          for ( let j = i + 1; j < array.length; j++){
+             if (!productOfTwo) {
+                 productOfTwo = array[i] * array[j] 
+                 continue;
+             }
              if ((array[i]* array[j]) > productOfTwo){
                 productOfTwo = (array[i]* array[j]);
              }
@@ -20,12 +24,15 @@ function highestProduct(array) {
      }
 console.log(productOfTwo)
      for (let k = 0; k < array.length; k++){
+        if (!productOfThree) {
+            productOfThree = productOfTwo * array[k] 
+            continue;
+        }
          if (productOfTwo * array[k] > productOfThree) {
              productOfThree = productOfTwo * array[k]
          }
      }
      return productOfThree;
 }
-console.log(highestProduct([1,-2,4]))
-
+console.log(highestProduct([-1,-3,-4,-2]))
 module.exports = highestProduct;
