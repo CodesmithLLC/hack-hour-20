60af4392dcb81b39e7661ed261b88b1e04ef28c3
JD <j@mesjohnson.com>
2018-03-21 09:47:16 -0700

    'highest-product' -- Fully Working

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,6 +3,7 @@
  */
 
 function highestProduct (arr) {
+  if (!Array.isArray(arr) || arr.length < 3) return 0
   const sorted = arr.sort();
   const end = arr.length - 1;
   // first two NEGATIVE numbers, and highest POSTIVE number
@@ -12,7 +13,8 @@ function highestProduct (arr) {
   return Math.max(case1, case2);
 }
 
-
-
+console.log(highestProduct("not an Array"))
+console.log(highestProduct([1, 2]))
+console.log(highestProduct([-5, -4, 1, 2, 3, 4]))
 
 module.exports = highestProduct;
