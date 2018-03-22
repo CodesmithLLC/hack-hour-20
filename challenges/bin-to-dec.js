6d6a10f4c7f8758049ca864ff2c365bae37e6ba0
pskchang <pschang@ucsd.edu>
2018-03-22 09:36:07 -0700

    Check test

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,41 @@
  */
 
 function binToDec(binary) {
+    let result = 0, exp = 1;
+    for (let i = binary.length - 1; i >= 0; i--) {
+        result = (parseInt(binary.charAt(i)) * exp) + result;
+        exp = exp * 2;
+    }
+    console.log(result);
+    return result;
 
 }
+binToDec('0')   //-> 0
+binToDec('11')  //-> 3
+binToDec('100') //-> 4
+binToDec('101') //-> 5
+binToDec('0101') //-> 5
+
+
+
+// function decToBin(decimal) {
+//     let result = 0, startExp = 1;
+//     for (let i = 0; i < decimal.length; i++) {
+//         let curr = parseInt(decimal.charAt(i));
+//         if (curr / exp > 2) {
+
+//         }
+//         result = (curr * exp) + result;
+//         exp = exp * 10;
+//     }
+//     console.log(result);
+
+// }
+// decToBin('0')  // -> 0
+// decToBin('3') // -> 3
+// decToBin('4')// -> 4
+// decToBin('5') //-> 5
+// decToBin('5') //-> 5
+ 
 
 module.exports = binToDec;
