a2cc7c0d90c52183112df0fed4f03df69dce7966
JK <jonathan.ko523@gmail.com>
2018-03-23 10:01:38 -0700

    test test

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,10 +7,30 @@
  */
 
 function Stack() {
-  let storage = [];
-  this.pop = storage.pop();
-  this.push = storage.push();
-  this.getMax = Math.max(...storage)
+  let storage = {}
+  storage.length = 0
+  storage.push = function(x){
+    storage[storage.length]=x
+    storage.length++
+  }
+  storage.pop = function(){
+    let returnval = storage[storage.length]
+    storage.delete(storage.length); 
+    storage.length--;
+    return returnval
+  }
+  storage.getMax = function(){
+    let Largest = 0
+    for(keys in storage){
+      console.log("FOR")
+      if(storage[keys] > Largest){
+        Largest = storage[keys];
+      }
+    }
+    return Largest
+  }
+  
+  return storage
 }
 
 module.exports = Stack;
