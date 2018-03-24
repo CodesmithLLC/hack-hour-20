7d043b1619eb80cb4f5d05e764cb92b773e00d87
Ben Gummelt <camaromelt@gmail.com>
2018-03-24 09:12:57 -0700

    Unfinished max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,34 @@
 
 function Stack() {
   // body...
+  this.storage = {};
+  this.count = 0;
+  this.push = (val) => {
+    this.storage[++this.count] = val;
+    return this.count; 
+  }
+  this.pop = () => {
+    if(this.count === 0){
+      return;
+    }
+   let output = this.storage[this.count];
+   delete this.storage[this.count];
+   this.count -= 1;
+   return output;
+  }
 }
 
 module.exports = Stack;
+let stack = new Stack;
+console.log(stack.push('taco'));
+console.log(stack.push('taco'));
+console.log(stack.push('beef supreme'));
+console.log(stack.push('enchilada'));
+console.log(stack.push('burrito'));
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
