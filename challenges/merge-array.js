277928475075734b40ab07b0b6a6b22cfb3916fe
pskchang <pschang@ucsd.edu>
2018-03-28 09:39:54 -0700

    Checking tests for merge-array

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,29 @@
  */
 
 function mergeArrays(arr1, arr2) {
+    let res = [];
+    let i = 0, j = 0;
+    // console.log(arr1[i]);
+    while (i < arr1.length && j < arr2.length) {
+        console.log(res);
+        if (arr1[i] < arr2[j]) {
+            res.push(arr1[i]);
+            i++;
+            console.log(res);
+        } else {
+            res.push(arr2[j]);
+            j++;
+        }
+    }
+    if (i < j) {
+        return res.concat(arr2.slice(j));
+    } else {
+        return res.concat(arr1.slice(i));
+    }}
 
-}
+var my_array = [3,4,6,10,11,15,21];
+var another_array = [1,5,8,12,14,19];
+
+console.log(mergeArrays(my_array, another_array));
 
 module.exports = mergeArrays;
