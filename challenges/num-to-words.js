fb29252366b91e4b2bb92f9955e65e07ed50a1aa
josephwu1994 <josephwu1994@gmail.com>
2018-03-29 10:28:58 -0700

    num-to-words

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -11,9 +11,49 @@
  * numToWords(300525151340440) -> 'ThreeHundredTrillionFiveHundredTwentyFiveBillionOneHundredFiftyOneMillionThreeHundredFortyThousandFourHundredForty'
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
+const threeDigit = (num) =>{
+	const oneDigit = num%10;
+	const twoDigit = Math.floor(num%100/10);
+	const threeDigit = Math.floor(num%1000/100);
+	let result = [];
+	const zeros = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+	const ones = ['Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Ninteen'];
+	const tens = ['','Ten', 'Twenty', 'Thirty', 'Fourty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninty'];
+	if(num === 0) return 'zero';
+	if(num % 1000 > 0) {
+		if(twoDigit === 1) result.unshift(ones[num%10]);
+		else if (twoDigit < 1) result.unshift(zeros[num%10]);
+		else if (num%10 !== 0) result.unshift(tens[twoDigit]+zeros[oneDigit]);
+		else result.unshift(tens[twoDigit]);
+		if(threeDigit !== 0) result.unshift(zeros[threeDigit]+'Hundred');
+	}
+	return result.join('');
+}
 
 function numToWords(num) {
+	
+	const final = [];
+	if (num / 1 % 1000) {
+		final.unshift(threeDigit(num%1000));
+	}
+	if (Math.floor(num / 1000) % 1000 !== 0) {
+		final.unshift(threeDigit(Math.floor(num/1000))+'Thousand');
+	}
+	if (Math.floor(num / 1000000) % 1000 !== 0) {
+		final.unshift(threeDigit(Math.floor(num /1000000)) +'Million');
+	}
+	if (Math.floor(num / 1000000000) % 1000 !== 0) {
+		final.unshift(threeDigit(Math.floor(num/1000000000)) +'Billion');
+	}
+	if (Math.floor(num / 1000000000000) % 1000 !== 0) {
+		final.unshift(threeDigit(Math.floor(num/1000000000000)) +'Trillion');
+	}
+	if (Math.floor(num / 1000000000000000) % 1000 !== 0) {
+		final.unshift(threeDigit(Math.floor(num /1000000000000000)) +'Quadrillion');
+	}
 
+	return final.join('');
 }
+console.log(numToWords(19231238084));
 
 module.exports = numToWords;
