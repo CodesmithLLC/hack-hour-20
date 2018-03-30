1c43d153bb4cceb6d0e5d13da84f8a9d0cdecbcc
Phillip Troutman <philliptroutman21@gmail.com>
2018-03-30 09:43:59 -0700

    solution for perm-palin.js

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,24 @@
  */
 
 function permPalin(str) {
-	
+    if(typeof str !== 'string') return false;
+    if (str.length === 1) return true;
+
+    for (let i = 0; i < str.length - 1; i++) {
+        if (str.slice(i + 1).indexOf(str[i]) !== -1) {
+            let subString = str.slice(i);
+            let revSubString = subString.split('').reverse().join('');
+            if (subString === revSubString) return true;
+        }
+    }//end of forloop
+    return false
 }
+
+// console.log(`should be true: `)
+// console.log(permPalin('abab'));
+// console.log(permPalin('cbaba'));
+// console.log(permPalin('a'));
+// console.log(`should be false: `)
+// console.log(permPalin('cbac'));
 
 module.exports = permPalin;
