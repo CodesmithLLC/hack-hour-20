feb7b5afa5318aad11888e5ac95de6b705e26c1b
Ben Gummelt <camaromelt@gmail.com>
2018-03-30 10:06:46 -0700

    Perm-Palin complete

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,25 @@
  */
 
 function permPalin(str) {
-	
+	if(str.length < 0){
+    return false;
+  } else if( str.length === 1){
+    return true;
+  }
+  let strArr = str.split('');
+  let key = {};
+  let length = strArr.length;
+  for(let i = 0; i < strArr.length; i++){
+    key[strArr[i]] = ++key[strArr[i]] || 1;
+  }
+  let newOdd = 0;
+for(let prop in key){
+  if(key[prop] % 2 === 1) newOdd++;
+  if(newOdd > 1){
+    return false;
+  }
+}
+return true;
 }
 
 module.exports = permPalin;
