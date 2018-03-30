454364080d83c492b7be2a73be0ae28f3863f9d2
Luis Ramirez <academieange@Luis-MacBook-Pro.local>
2018-03-30 09:47:34 -0700

    palin

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,47 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
+ // input: a number
+ //output: a string represntation of the number
+
+ // we must get the place value of the number
+ //convert number to a string and get lenght
+ // create array of number strings
+ // declare a varaible place value equal 10 to the power of length -1
+ // create object of number : wordNumber value pairs
+ // declare a string result acumulator 
+  // iterate from the front of array of numberStrings
+  // find value of string number add place value
+
+
 function numToWords(num) {
+    let pow = num.toString().length-1;
+    let placeValue = Math.pow(10,pow);
+    let arrayOfNum = num.toString().split('');
+    let objNumToWords = {
+        '1': 'one',
+        '2': 'two',
+        '3': 'three',
+        '4': 'four',
+        '5': 'five',
+        '6': 'six',
+        '7':'seven',
+        '8':'eight',
+        '9': 'nine',
+        '100':'hundred',
+        '1000': 'thousand',
+    };
+    let resultAcc = '';
+    for ( let i = 0; i < arrayOfNum.length; i++){
+        if (arrayOfNum[i] === '0') continue;
+
+        resultAcc += objNumToWords[arrayOfNum[i]] + objNumToWords[Math.pow(10,pow)];
+        pow--;
+    }
+
+return resultAcc
 
 }
+console.log(numToWords(9168))
 
 module.exports = numToWords;
--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -8,9 +8,38 @@
  * 	- permPalin('a') => true
  *
  */
-
+// make permutations
+//compare to reverse
 function permPalin(str) {
-	
+let objCharCount = {};
+    let arrStr = str.split('');
+    for ( let i = 0; i < arrStr.length; i++ ){ 
+        if (!objCharCount[arrStr[i]]) {
+            objCharCount[arrStr[i]] = 1;
+        } else {
+            objCharCount[arrStr[i]]++
+        }
+    //     let removed = arrStr.pop();
+    //   arrStr.unshift(removed)
+    //   console.log(arrStr);
+    //   console.log('reverse',arrStr.reverse())
+    //   if (JSON.stringify(arrStr) === JSON.stringify(arrStr.reverse())) return true;
+  }
+let charCount = Object.values(objCharCount);
+let odd = 0;
+  console.log(Object.values(objCharCount))
+  charCount.filter((num)=> {
+      if (num %2 !== 0) {
+          odd++;
+      }
+  });
+
+  if (odd > 1){
+       return false;
+  } else {
+      return true;
+  }
 }
+console.log(permPalin('a'))
 
 module.exports = permPalin;
