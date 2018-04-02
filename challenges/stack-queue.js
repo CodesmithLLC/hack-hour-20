8da2c1e5ea319569a97903f3f9dd60764d89d5c4
Madalyn Baehre <mmb296@cornell.edu>
2018-04-02 09:55:10 -0700

    stack-queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,8 +4,23 @@
 
 
 function Stack() {
-
-}
+    this.index = 0;
+    this.stack = {};
+    
+    this.push = function(value) {
+        this.stack[this.index] = value;
+        this.index++;
+        return this.index;
+    };
+    
+    this.pop = function() {
+        this.index--;
+        let toRemove = this.stack[this.index];
+        delete this.stack[this.index];
+        return toRemove;
+    };
+    
+    }
 
 
 /**
@@ -14,7 +29,32 @@ function Stack() {
 
 
 function Queue() {
-
-}
+    this.stack1 = new Stack();
+    this.stack1length = 0;
+    this.stack2 = new Stack();
+    
+    this.push = function(value) {
+      this.stack1length = this.stack1.push(value);
+      return this.stack1length;
+    };
+    
+    this.pop = function() {
+      for (let i = 0; i < this.stack1length; i++) {
+        let addVal = this.stack1.pop();
+        this.stack2.push(addVal);
+      }
+      let returnVal = this.stack2.pop();
+      for (let i = 0; i < this.stack1length; i++) {
+        let addVal = this.stack2.pop();
+        this.push(addVal);
+      }
+      return returnVal;
+       
+    };
+    
+    }
 
 module.exports = {Stack: Stack, Queue: Queue};
+
+
+console.log(Stack.push(2));
