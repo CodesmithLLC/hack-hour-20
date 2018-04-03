778ba3f0f1eee5768c509532a0ac164bf8cbf861
josephwu1994 <josephwu1994@gmail.com>
2018-04-03 09:34:59 -0700

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,15 @@
  */
 
 function twoSum(arr, n) {
-
+	if(arr.length === 0 || n === undefined || !Array.isArray(arr)) {
+		return false;
+	}
+	for (let i = 0 ; i < arr.length; i++) {
+		const num = n - arr[i];
+		const test = [...arr.slice(0,i), ...arr.slice(i+1)];
+		if(test.includes(num)) return true;
+	}
+	return false;
 }
 
 module.exports = twoSum;
