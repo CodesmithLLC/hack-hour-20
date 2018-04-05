fff5f8648b46666260871e31c0715131511a840e
Madalyn Baehre <mmb296@cornell.edu>
2018-04-05 09:24:34 -0700

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,16 @@
  */
 
 function gcd(a, b) {
-
+  if (a === b) {
+    return a;
+  }
+  let difference = Math.abs(a - b);
+  for (let i = difference; i > 1; i--) {
+    if (a%i === 0 && b%i === 0) {
+      return i;
+    }
+  }
+  return 1;
 }
 
 module.exports = gcd;
