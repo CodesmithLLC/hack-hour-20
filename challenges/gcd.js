206f09a4c58630fa281b1007ad6d7e604a335264
JD <j@mesjohnson.com>
2018-04-05 10:35:23 -0700

    HH20 Reallllly Updated

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -9,15 +9,15 @@
 
 function gcd (a, b) {
   let max = Math.max(a, b), min = Math.min(a, b);
-  return max % min === 0 ? min : mod;
+  return max % min === 0 ? min : max % min;
 }
 
-function gcd1Liner (a, b) {
-  return Math.max(a, b) % Math.min(a, b) === 0 ? Math.min(a, b) : Math.max(a, b) % Math.min(a, b);
-}
+// function gcd1Liner (a, b) {
+//   return Math.max(a, b) % Math.min(a, b) === 0 ? Math.min(a, b) : Math.max(a, b) % Math.min(a, b);
+// }
 
 console.log(gcd(10, 8))
 console.log(gcd(8, 10))
-console.log(gcd(120, 60))
-console.log("mod", 2 % 10)
+console.log(gcd(123, 60))
+
 module.exports = gcd;
