e947bc76d37331afd4a94ac4aabbbdce9151bc1a
Phillip Troutman <philliptroutman21@gmail.com>
2018-04-19 09:28:32 -0700

    repeat number withough the space complexity bonus finished

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -1,5 +1,5 @@
 /**
- * I have an array where every number in the range 1...n appears once except for 
+ * I have an array where every number in the range 1...n appears once except for
  * one number which appears twice.
  *
  * Write a function for finding the number that appears twice.
@@ -11,7 +11,15 @@
  */
 
 function repeatNumbers(array) {
-
+    let result = {};
+    for (let i = 0; i < array.length; i++){
+        if(result[array[i]]){
+            return array[i];
+        }
+        result[array[i]] = 'foo';
+    }
 }
+
+console.log(repeatNumbers([1,2,3,7,3,4,6,9]));
 
 module.exports = repeatNumbers;
