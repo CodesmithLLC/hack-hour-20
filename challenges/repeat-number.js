a64971541ce0fb4cff5f777418d84c0ef1016172
Luis Ramirez <academieange@Luis-MacBook-Pro.local>
2018-04-19 09:51:23 -0700

    repeatnumber

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,17 @@
  */
 
 function repeatNumbers(array) {
-
+    let duplicateNumber = [];
+    array.forEach((number, index)=> {
+        if (array.indexOf(number, index +1) > -1){
+            if (duplicateNumber.indexOf(number) === -1){
+                duplicateNumber.push(number);
+            }
+        }
+    })
+    return duplicateNumber[0];
 }
+
+console.log(repeatNumbers([1,2,3,4,5,6,7,8,9,4,10]));
 
 module.exports = repeatNumbers;
