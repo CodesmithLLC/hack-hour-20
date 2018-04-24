495ca2341f807e646d7919294968287d99e36e4c
Ben Gummelt <camaromelt@gmail.com>
2018-04-24 10:30:57 -0700

    Completed Linked List and Max Subarray

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -27,22 +27,30 @@
  *
  */
 
-var Node = function(value) {
+const Node = function(value) {
   this.value = value;
   this.next = null;
-}
+};
 
 function hasCycle(head) {
-
+  let a = head;
+  let b = head;
+  while (a && b) {
+    if (b.next == null) return false;
+    a = a.next;
+    b = b.next.next;
+    if (a === b) return true;
+  }
+  return false;
 }
 
-module.exports = {Node: Node, hasCycle: hasCycle}
+module.exports = {Node: Node, hasCycle: hasCycle};
 
-//  const node1 = new Node('1');
-//  const node2 = node1.next = new Node('2');
-//  const node3 = node2.next = new Node('3');
-//  const node4 = node3.next = new Node('4');
-//  const node5 = node4.next = new Node('5');
-//  console.log(hasCycle(node1)); // => false
-//  node5.next = node2;
-//  console.log(hasCycle(node1)); // => true
+const node1 = new Node('1');
+const node2 = node1.next = new Node('2');
+const node3 = node2.next = new Node('3');
+const node4 = node3.next = new Node('4');
+const node5 = node4.next = new Node('5');
+console.log(hasCycle(node1)); // => false
+node5.next = node2;
+console.log(hasCycle(node1)); // => true
