3b8430c76777b0c8152f6dd1c755c8fb0dbbf120
Ben Gummelt <camaromelt@gmail.com>
2018-04-24 09:06:37 -0700

    Completed 3 or 5

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -38,11 +38,11 @@ function hasCycle(head) {
 
 module.exports = {Node: Node, hasCycle: hasCycle}
 
- const node1 = new Node('1');
- const node2 = node1.next = new Node('2');
- const node3 = node2.next = new Node('3');
- const node4 = node3.next = new Node('4');
- const node5 = node4.next = new Node('5');
- console.log(hasCycle(node1)); // => false
- node5.next = node2;
- console.log(hasCycle(node1)); // => true
+//  const node1 = new Node('1');
+//  const node2 = node1.next = new Node('2');
+//  const node3 = node2.next = new Node('3');
+//  const node4 = node3.next = new Node('4');
+//  const node5 = node4.next = new Node('5');
+//  console.log(hasCycle(node1)); // => false
+//  node5.next = node2;
+//  console.log(hasCycle(node1)); // => true
