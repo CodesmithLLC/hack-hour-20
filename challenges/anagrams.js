a4d4f97a2bd6b95a56e144c3bbf7ee2ff4ccd2ed
Madalyn Baehre <mmb296@cornell.edu>
2018-04-25 09:38:44 -0700

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,23 @@
   */
 
 function anagrams(string) {
-
+  let output = [];
+  if (string.length === 1) {
+    output.push(string[0]);
+    return output;
+  }
+  for (let i = 0; i < string.length; i++) {
+    for (let j = 0; j < string.length; j++) {
+      let newStr = string[i];
+      if (j < i) {
+        newStr = newStr + string.slice(j, i) + string.slice(i + 1, string.length) + string.slice(0, j);
+        output.push(newStr);
+      } else if (j > i) {
+        newStr = newStr + string.slice(j, string.length) + string.slice(0, i) + string.slice(i + 1, j);
+        output.push(newStr);
+      }
+    }
+  }
+  return output;
 }
-
 module.exports = anagrams;
