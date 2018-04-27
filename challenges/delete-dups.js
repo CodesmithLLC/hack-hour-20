23191745e52504b096521b0e4ced161eedc2931a
Phillip Troutman <philliptroutman21@gmail.com>
2018-04-27 15:37:44 -0700

    hackhour for 4/26

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,37 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
+function Node (val) {
+    this.value = val;
+    this.next = null;
+}
+
+let phillip = new Node(1);
+phillip.next = new Node(3);
+phillip.next.next = new Node(4)
+phillip.next.next.next = new Node(4)
+phillip.next.next.next.next = new Node(0)
+phillip.next.next.next.next.next = new Node(4)
+console.log(phillip, '<---phillip');
+
 
 
 function deleteDups(head) {
+    let buffer = {};
+    let current = head;
+    let prev = null;
+    while(current !== null){
+        if(buffer.hasOwnProperty(current.value)){
+            prev.next = current.next;
+            console.log('removed');
+        }else{
+            buffer[current.value] = 1;
+        }
+        prev = current
+        current = current.next
+    }
+    return head
 
 }
-
+console.log(JSON.stringify(deleteDups(phillip)));
 module.exports = deleteDups;
