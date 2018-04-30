bf960364f383185a3c4123e390e06cbdf33bb014
josephwu1994 <josephwu1994@gmail.com>
2018-04-30 11:30:59 -0700

    Insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,17 @@
 
 
 function insertionSort(array) {
-
+	for(let i = array.length-1; i >= 0; i--) {
+		array.forEach((num, i) => {
+			if(array[i+1] < num && i < array.length-1) {
+				const temp = array[i+1];
+				array[i+1] = num;
+				array[i] = temp;
+			}
+		})
+	}
+	return array;
 }
+
 
 module.exports = insertionSort;
