a3f529496d3801cec9f2bfaf3ebde9c8a0164daa
Phillip Troutman <philliptroutman21@gmail.com>
2018-05-04 08:22:21 -0700

    doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -15,16 +15,58 @@ function Node(val) {
 
 /*
 Adds a node to the end of the list
- */
+*/
 LinkedList.prototype.add = function(val) {
-  
+  if(!this.head && !this.tail){
+    this.head = new Node(val);
+    this.tail = this.head
+    // this.tail.prev = this.head;
+  }else{
+    let last = this.tail;
+    this.tail = new Node(val);
+    last.next = this.tail;
+    this.tail.prev = last;
+  }
 };
 
 /*
 Removes the first node with the inputted value
- */
+*/
 LinkedList.prototype.remove = function(val) {
-  
+  //set a current to keep track of where you are in the list
+  let current = this.head;
+  //while there is another node in the list keep going
+  while(this.head.val){
+    //check if the current node is the one your need to remove
+    if(current.val === val){
+      //Before deleting check if its the head node.
+      //If it is reset the head node
+      if(current === this.head){
+        this.head = current.next;
+      }
+      //Before deleting check if its the tail node.
+      //If it is reset the tail node
+      if(current === this.tail){
+        this.tail === current.prev;
+      }
+      //Delete current node by removing its references in the list
+      current.prev.next = current.next;
+      return;
+    }
+      current = current.next
+  }
 };
 
+let phillip = new LinkedList();
+
+phillip.add('yo');
+phillip.add('taco');
+phillip.add('water');
+phillip.add('coffee');
+phillip.add('burrito');
+phillip.remove('taco');
+console.log(phillip, '<----phillip');
+
+console.log('===test===')
+// console.log(phillip.tail.prev.prev.val);
 module.exports = LinkedList;
--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,45 +14,10 @@
   ****
  *****
 ******
-
 */
 
+/////first attempt/////
 
-//refactor #1
-// function drawStairs(n){
-//     let result = "";
-//     let space = " "
-//     let star = "*";
-//     let newLine = "\n";
-//     for(let i = 1; i <= n; i++){
-//         result +=space.repeat(n-i);
-//         result += star.repeat(i);
-//         result += newLine;
-//     }//end of forLoop
-//     return result;
-// }
-
-
-
-// console.log(drawStairs(6));
-
-
-// module.exports = drawStairs;
-
-
-//////////////////////////////////////////////////////////////////////
-//Just some simple recurrsion practice
-function sum(...args) {
-    //base case
-    if (args.length === 2) {
-        return args[0] + args[1];
-    }
-    return sum(args[0] + args[1], ...args.slice(2))
-}
-// console.log(sum(1,2,3,4));//===> shold be 10
-
-
-//first try
 // function drawStairs(n) {
 //     //create the starting point with enough spaces before the *
 //     let startPoint = "*";
@@ -71,22 +36,83 @@ function sum(...args) {
 //     }
 // }
 
+/////refactor #1/////
 
-//refactor #2 (recurssive);
-function drawStairs(n, counter = 0, carryover = '') {
-    //base case
-    if(counter === n){
-        return carryover;
-    }
-    const addStar = (str, wsCount, starCount) => {
-        //?????*\n????**\n???***\n??****\n?*****\n******\n
-        str += ' '.repeat(wsCount);
-        str += '*'.repeat(starCount);
-        str+= '\n';
-        return str;
-    }
-    return drawStairs(n, ++counter, addStar(carryover, n - counter, counter));
-}
+// function drawStairs(n){
+//     let result = "";
+//     let space = " "
+//     let star = "*";
+//     let newLine = "\n";
+//     for(let i = 1; i <= n; i++){
+//         result +=space.repeat(n-i);
+//         result += star.repeat(i);
+//         result += newLine;
+//     }//end of forLoop
+//     return result;
+// }
+
+/////refactor #2 (recurssive)/////
+// function drawStairs(n, counter = 0, carryover = '') {
+//     //base case
+//     if (counter === n) {
+//         return carryover;
+//     }
+//     const addStar = (str, wsCount, starCount) => {
+//         //?????*\n????**\n???***\n??****\n?*****\n******\n
+//         str += ' '.repeat(wsCount);
+//         str += '*'.repeat(starCount);
+//         str += '\n';
+//         return str;
+//     }
+//     return drawStairs(n, ++counter, addStar(carryover, n - counter, counter));
+// }
+
+/////CS SOLUTIONS/////
+//optimized for time
+// function makeSpaceArray(character, x) {
+//     var str = [];
+//     for (var i = 0; i < x; i++) {
+//       str.push(character);
+//     }
+//     return str;
+//   }
+//   function drawStairs(n) {
+//     var arr = makeSpaceArray(' ', n);
+//     while (n > 0) {
+//       arr[--n] = '*';
+//       console.log(arr.join(''));
+//     }
+//   }
+
+//   //optimized for space
+//   function drawStairs(n) {
+//     for (var i = 0; i < n; i++) {
+//       for (var j = 0; j < n; j++) {
+//         process.stdout.write(j + i + 1 >= n ? '*' : ' ');
+//       }
+//       process.stdout.write('\n');
+//     }
+//   }
+//   //optimized for both
+//   function drawStairs(n) {
+//     for (var i = 1; i <= n; i++) {
+//       var spaces = ' '.repeat(n - i);
+//       var splats = '*'.repeat(i);
+//       console.log(spaces + splats);
+//     }
+//   }
+
+console.log(drawStairs(6));
+module.exports = drawStairs;
 
 
-console.log(drawStairs(60))
+//////////////////////////////////////////////////////////////////////
+//Just some simple recurrsion practice
+// function sum(...args) {
+//     //base case
+//     if (args.length === 2) {
+//         return args[0] + args[1];
+//     }
+//     return sum(args[0] + args[1], ...args.slice(2))
+// }
+// console.log(sum(1,2,3,4));//===> shold be 10
