fd08737e910c6d4705027b4d6c9b269ddd619268
Ben Gummelt <camaromelt@gmail.com>
2018-05-05 09:46:15 -0700

    Completed Phonebook

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -3,13 +3,15 @@
 * You are given an array of arrays.  The subarrays are first names and phone numbers.
 *
 * Example:
-*   jazbook = [
-*     ['alex','301-844-3421'],
-*     ['jae','301-844-1211'],
-*     ['david','301-844-0978'],
-*     ['travis','301-844-8505']
-*     ['jasmine','1800-974-4539'],
-*   ];
+**/
+const jazbook = [
+  ['alex', '301-844-3421'],
+  ['jae', '301-844-1211'],
+  ['david', '301-844-0978'],
+  ['travis', '301-844-8505'],
+  ['jasmine', '1800-974-4539'],
+];
+/**
 *
 * jazbooks are not always sorted...
 *
@@ -25,15 +27,32 @@
 */
 
 //  return the number associated with the name in the jazbook
-function findName(jazbook, name) {
-
+function findName(arr, name) {
+  let result = false;
+  let res;
+  for (let i = 0, j = arr.length - 1; i < Math.floor(arr.length / 2); i += 1, j -= 1) {
+    if (arr[i][0] === name) {
+      result = true;
+      res = arr[i][1];
+    }
+    if (arr[j][0] === name) {
+      result = true;
+      res = arr[j][1];
+    }
+    if (result) break;
+  }
+  return result ? res : result;
 }
-
+console.log(findName(jazbook, 'alex'));
 // return an object literal representing the jazbook
-function makePhoneBookObject(jazbook){
-
+function makePhoneBookObject(arr) {
+  const result = {};
+  arr.forEach(([name, num]) => {
+    result[name] = num;
+  });
+  return result;
 }
-
+console.log(makePhoneBookObject(jazbook));
 const objectToExport = {
   findName,
   makePhoneBookObject,
