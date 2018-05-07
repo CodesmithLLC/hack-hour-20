621d63660154cd8f97bf4ee8d9f84ee6c611599c
Madalyn Baehre <mmb296@cornell.edu>
2018-05-07 09:25:09 -0700

    bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,19 @@
 
 
 function bubbleSort(array) {
-
+  for (let i = array.length; i >= 0; i--) {
+    let count = 0;
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j+1]) {
+        let temp = array[j+1];
+        array[j+1] = array[j];
+        array[j] = temp;
+        count++;
+      }
+    }
+    if (count === 0) {
+      return array;
+    }
+  }
 }
-
 module.exports = bubbleSort;
