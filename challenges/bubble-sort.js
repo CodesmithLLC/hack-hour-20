53af9b974c8e2a9235a0eafa9118f0dadc4112f3
Ben Gummelt <camaromelt@gmail.com>
2018-05-07 09:29:40 -0700

    Bubble Sort Done

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,18 @@
 
 
 function bubbleSort(array) {
-
+  let count = array.length;
+  while (count !== 0) {
+    for (let i = 0; i < count - 1; i += 1) {
+      if (array[i] > array[i + 1]) {
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+    count -= 1;
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
