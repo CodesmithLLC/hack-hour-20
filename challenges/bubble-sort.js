311e14c8cf814e2e463484c762144b555ae54b0a
josephwu1994 <josephwu1994@gmail.com>
2018-05-07 09:47:42 -0700

    bubble sort + insertion sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,16 @@
 
 
 function bubbleSort(array) {
-
+	for(let i = array.length-1; i >= 0; i--) {
+		array.forEach((num, i) => {
+			if(array[i+1] < num && i < array.length-1) {
+				const temp = array[i+1];
+				array[i+1] = num;
+				array[i] = temp;
+			}
+		})
+	}
+	return array;
 }
 
 module.exports = bubbleSort;
